897e21e12359591630933a61fa0e55afad4c0bde
Md Sultan Mahmud <160622461+HiIamSultan@users.noreply.github.com>
2024-11-30 00:48:18 -0500

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,22 +8,4 @@ document.querySelectorAll('.abstract-toggle').forEach(button => {
     });
 });
 
-document.querySelectorAll('.dropdown-toggle').forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        const dropdown = toggle.nextElementSibling;
-        dropdown.classList.toggle('show');
-    });
-});
-
-document.querySelectorAll('.navbar li a').forEach(link => {
-    link.addEventListener('click', event => {
-        const dropdown = link.nextElementSibling;
-
-        if (dropdown && dropdown.tagName === 'UL') {
-            event.preventDefault(); // Prevent default link behavior
-            dropdown.classList.toggle('show');
-        }
-    });
-});
-
 
